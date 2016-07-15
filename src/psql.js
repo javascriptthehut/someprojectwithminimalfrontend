17118d9528f1b050960fd5a6f04b7db5df6be184
nogainbar <nogaenbar@MBPro-2.local>
2016-07-15 08:41:21 +0100

    more comments1

--- src/psql.js
+++ src/psql.js
@@ -1,5 +1,9 @@
+
+//setting relation to postgres db
+
 const pg = require('pg');
 let DB = '';
+
 if (process.env.DATABASE_URL){      //heroku
   DB = process.env.DATABASE_URL;
   pg.defaults.ssl = true;
@@ -7,6 +11,7 @@ if (process.env.DATABASE_URL){      //heroku
   DB = 'postgres:///tweetdb';
 }
 
+//sets a new postgres client
 const client = new pg.Client(DB);
 console.log(`connecting to ${DB}...`);
 client.connect((err) => {
@@ -14,4 +19,5 @@ client.connect((err) => {
   console.log(`connected to ${DB}`);
 });
 
+//mosule will be used in urlpaths as 'client.' in order to establish connection to db
 module.exports = client;
